01738fa829b1a43bec59fad50511ea766e63112b
Sephiroth018 <andreas_neuwirth@hotmail.com>
2014-07-23 20:00:06 +0200

    updated deployment file

--- DevDeploy.cmd
+++ DevDeploy.cmd
@@ -1,12 +1,11 @@
 D:
-cd "D:\GitHub\ESO-Addons\GuildStoreSearchExtendedFix"
+cd "D:\GitHub\ESO-Addons\GuildStoreSearchExtended"
 
 rmdir /s /q "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\"
 
 xcopy info "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\info\" /Y /I /E
 xcopy lang "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\lang\" /Y /I /E
-xcopy LibStub "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\LibStub\" /Y /I /E
-xcopy LibAddonMenu-1.0 "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\LibAddonMenu-1.0\" /Y /I /E
+xcopy Libs "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\Libs\" /Y /I /E
 xcopy *.lua "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\" /Y
 xcopy *.txt "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\" /Y
 xcopy *.xml "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\GuildStoreSearchEx\" /Y
